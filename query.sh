88da40cb1d54783f0c939bf15247e5d425baadc0
kaylanamira <kaylanmr04@gmail.com>
2026-07-19 12:34:00 +0700

    feat: adjust query sh

--- query.sh
+++ query.sh
@@ -128,7 +128,7 @@ elif [ "${1:-}" = "--chart2" ]; then
 JSON
 )
 else
-  QUERY="${1:-apakah ada isu yg dialamai mahasiswa untuk mata kuliah IF3170?}"
+  QUERY="${1:-bagaimana pelaksanaan perkuliahan mata kuliah if3170 ?}"
   BODY=$(cat <<JSON
 {
   "query": "${QUERY}",
